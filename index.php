c7de25fe01c25f523184cf5bbcbcddd6a09801fc
Alberto Serafini <ilgumo@gmail.com>
2024-01-10 17:02:12 -0500

    created the php file

---
+++ index.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+</head>
+<body>
+    
+<h1> 
+    
+    <?php echo " riga in php dentro HTML"; ?>
+    
+    </h1>
+</body>
+</html>
